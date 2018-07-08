f081ac805e8900d3e4259df289911904c91209a1
sbx_user1060 <sbx_user1060@ip-10-12-251-179.eu-west-2.compute.internal>
2018-07-08 12:54:05 +0000

    Sun Jul 08 12:54:05 UTC 2018

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,16 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+
 def compute(x, y):
-    raise NotImplementedError()
+    """
+    Return the sum of two integers.
+
+    param x: a positive integer between 0-100
+    param y: a positive integer between 0-100
+    @return: an Integer representing the sum of the two numbers
+    """
+    if not (isinstance(x, int) and isinstance(y, int)):
+        return 'Please enter only integers, example: 40'
+    if not ((x >= 0 and x <= 100) and (y >= 0 and y <= 100)):
+        return 'Please enter only integers between 0 and 100.'
+    return x + y
