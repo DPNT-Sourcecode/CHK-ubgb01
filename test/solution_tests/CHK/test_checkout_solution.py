7dc5d0ee6c1899633d08c84df39f16b966d826e0
sbx_user1061 <sbx_user1061@ip-10-11-229-161.eu-west-2.compute.internal>
2018-07-08 22:10:17 +0000

    Sun Jul 08 22:10:17 UTC 2018

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -8,8 +8,10 @@ class TestCheckout(unittest.TestCase):
     def test_illegal_input(self):
         self.assertEqual(checkout_solution.illegal_input('ABCY'), True)
 
-    # def test_checkout(self):
-    #     self.assertEqual(checkout_solution.checkout(self.friend_name), )
+    def test_checkout(self):
+        self.assertEqual(checkout_solution.checkout('AAABCCC'), 220)
+        self.assertEqual(checkout_solution.checkout('AABBCDD'), 195)
+        self.assertEqual(checkout_solution.checkout('AABbCDD'), -1)
 
 
 if __name__ == '__main__':
