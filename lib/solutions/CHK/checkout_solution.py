fa17cae05ec33b1fddc90aa6336a00ef50291a0a
sbx_user1061 <sbx_user1061@ip-10-11-229-161.eu-west-2.compute.internal>
2018-07-08 21:55:17 +0000

    Sun Jul 08 21:55:17 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,25 @@
+from collections import Counter
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    param skus: a String containing the SKUs of all the 
+                products in the basket.
+    @return:    an Integer representing the total checkout
+                value of the items.
+    """
+    total_checkout = 0
+    sku_table = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15,
+    }
+    for letter in skus:
+        try:
+            total_checkout += sku_table[letter]
+        except KeyError:
+            return -1
+    return total_checkout 
