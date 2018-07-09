7adb9ea29ae930767abca525c9e9cfd844825533
sbx_user1061 <sbx_user1061@ip-10-12-133-10.eu-west-2.compute.internal>
2018-07-09 20:02:49 +0000

    Mon Jul 09 20:02:49 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -35,7 +35,8 @@ def checkout(skus):
     """
     total_checkout = 0
     quantity = {}
-
+    so_5A = False
+    
     if illegal_input(skus):
         return -1
 
@@ -45,10 +46,17 @@ def checkout(skus):
         except KeyError:
             quantity[item] = 1
 
-        if item == 'A' and quantity[item] % 3 == 0:
-            total_checkout += PRICE_TABLE[item] - 20
-        elif item == 'A' and quantity[item] % 5 == 0:
-            total_checkout += PRICE_TABLE[item] - 30
+        if item == 'A' and quantity[item] % 5 == 0:
+            # add back money removed from special offer 3A
+            total_checkout += 20
+            # activate special offer 5A
+            so_5A = True
+        elif item == 'A' and quantity[item] % 3 == 0:
+            if so_5A:
+                total_checkout += PRICE_TABLE[item]
+                so_5A = False
+            else:
+                total_checkout += PRICE_TABLE[item] - 20
         elif item == 'B' and quantity[item] % 2 == 0:
             total_checkout += PRICE_TABLE[item] - 15
         elif item == 'E' and quantity[item] % 2 == 0:
--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -13,12 +13,19 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(checkout_solution.checkout('EEBB'), 95)
         self.assertEqual(checkout_solution.checkout('BAB'), 95)
         self.assertEqual(checkout_solution.checkout('AAAA'), 180)
+
         self.assertEqual(checkout_solution.checkout('AEEC'), 120)
         self.assertEqual(checkout_solution.checkout('CDBEA'), 155)
         self.assertEqual(checkout_solution.checkout('AAAAA'), 200)
+ 
+        self.assertEqual(checkout_solution.checkout('AAAAAA'), 250)
         self.assertEqual(checkout_solution.checkout('BBBBB'), 120)
         self.assertEqual(checkout_solution.checkout('AAABBD'), 190)
-        self.assertEqual(checkout_solution.checkout('AAAAAA'), 230)
+
+        self.assertEqual(checkout_solution.checkout('AAAAAA'), 250)
+        self.assertEqual(checkout_solution.checkout('AAAAAAA'), 300)
+        
+        self.assertEqual(checkout_solution.checkout('AAAAAAAAA'), 380)
         self.assertEqual(checkout_solution.checkout('AABBCDD'), 195)
         self.assertEqual(checkout_solution.checkout('AAAAABCC'), 270)
         self.assertEqual(checkout_solution.checkout('AABbCDDea'), -1)
