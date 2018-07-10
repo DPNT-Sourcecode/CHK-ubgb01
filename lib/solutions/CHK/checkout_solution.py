b69c1a31ca01ef35250118d41dd7af456d5897ec
sbx_user1060 <sbx_user1060@ip-10-12-119-22.eu-west-2.compute.internal>
2018-07-10 11:50:28 +0000

    Tue Jul 10 11:50:28 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,26 @@ PRICE_TABLE = {
     'D': 15,
     'E': 40,
     'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50
 }
 
 
@@ -34,17 +54,12 @@ def checkout(skus):
     @return:    an Integer representing the total checkout
                 value of the items.
     """
+    quantity = {}
     total_checkout = 0
     free_b_count = 0
     free_f_count = 0
-    quantity = {
-        'A': 0,
-        'B': 0,
-        'C': 0,
-        'D': 0,
-        'E': 0,
-        'F': 0,
-    }
+    free_m_count = 0
+    free_q_count = 0
     
     if illegal_input(skus):
         return -1
@@ -53,14 +68,56 @@ def checkout(skus):
     skus = ''.join(sorted(skus, reverse=True))
 
     for item in skus:
-        quantity[item] += 1
+        try:
+            quantity[item] += 1
+        except KeyError:
+            quantity[item] = 1
         
-        if item == 'F' and quantity[item] % 2 == 0:
-            free_f_count +=1
+        # 3V for 130
+        if item == 'V' and quantity[item] % 3 == 0:
+            total_checkout += PRICE_TABLE[item] - 10
+            quantity[item] = 0
+        # 2V for 90
+        elif item == 'V' and quantity[item] % 2 == 0:
+            total_checkout += PRICE_TABLE[item] - 10
+        # 3R get one Q free
+        elif item == 'R' and quantity[item] % 3 == 0:
+            free_q_count += 1
+            total_checkout += PRICE_TABLE[item]
+        elif item == 'Q' and free_q_count:
+            free_q_count -= 1
+            quantity[item] = 0
+        # 3Q for 80
+        elif item == 'Q' and quantity[item] % 3 == 0:
+            total_checkout += PRICE_TABLE[item] - 10
+        # 5P for 200
+        elif item == 'P' and quantity[item] % 5 == 0:
+            total_checkout += PRICE_TABLE[item] - 50
+        # 3N get one M free
+        elif item == 'N' and quantity[item] % 3 == 0:
+            free_m_count += 1
+            total_checkout += PRICE_TABLE[item]
+        elif item == 'M' and free_m_count:
+            free_m_count -= 1
+            quantity[item] = 0
+        # 2K for 150
+        elif item == 'K' and quantity[item] % 2 == 0:
+            total_checkout += PRICE_TABLE[item] - 10
+        # 10H for 80
+        elif item == 'H' and quantity[item] % 10 == 0:
+            total_checkout += PRICE_TABLE[item] - 15
+            quantity[item] = 0
+        # 5H for 45
+        elif item == 'H' and quantity[item] % 5 == 0:
+            total_checkout += PRICE_TABLE[item] - 5
+        # 2F get one F free
+        elif item == 'F' and quantity[item] % 2 == 0:
+            free_f_count += 1
             total_checkout += PRICE_TABLE[item]
         elif item == 'F' and free_f_count:
             free_f_count -= 1
             quantity[item] = 0
+        # 2E get one B free
         elif item == 'E' and quantity[item] % 2 == 0:
             # keep count of how may B items shopper should get
             free_b_count += 1
@@ -68,13 +125,16 @@ def checkout(skus):
         elif item == 'B' and free_b_count:
             free_b_count -=1 
             quantity[item] = 0
+        # 2B for 45
         elif item == 'B' and quantity[item] % 2 == 0:
             total_checkout += PRICE_TABLE[item] - 15
+        # 5A for 200
         elif item == 'A' and quantity[item] % 5 == 0:
             # add back money removed from special offer 3A
             total_checkout += 20
             # reset count
             quantity[item] = 0
+        # 3A for 130
         elif item == 'A' and quantity[item] % 3 == 0:
             total_checkout += PRICE_TABLE[item] - 20
         else:
--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -6,7 +6,7 @@ from solutions.CHK import checkout_solution
 class TestCheckout(unittest.TestCase):
 
     def test_illegal_input(self):
-        self.assertEqual(checkout_solution.illegal_input('ABCY'), True)
+        self.assertEqual(checkout_solution.illegal_input('ABCi'), True)
         self.assertEqual(checkout_solution.illegal_input('ABCD'), False)
 
     def test_checkout(self):
@@ -38,19 +38,58 @@ class TestCheckout(unittest.TestCase):
         
         self.assertEqual(checkout_solution.checkout('AAAAABCC'), 270)
         self.assertEqual(checkout_solution.checkout('AABbCDDea'), -1)
-
         self.assertEqual(checkout_solution.checkout('F'), 10)
         self.assertEqual(checkout_solution.checkout('FF'), 20)
+
         self.assertEqual(checkout_solution.checkout('FFF'), 20)
         self.assertEqual(checkout_solution.checkout('FFB'), 50)
         self.assertEqual(checkout_solution.checkout('FFFF'), 30)
         self.assertEqual(checkout_solution.checkout('FFFFF'), 40)
+        
         self.assertEqual(checkout_solution.checkout('FFFFFF'), 40)
         self.assertEqual(checkout_solution.checkout('FFFFFFFF'), 60)
         self.assertEqual(checkout_solution.checkout('FFFFFFFFF'), 60)
         self.assertEqual(checkout_solution.checkout('FFFFFFFFFF'), 70)
+        
         self.assertEqual(checkout_solution.checkout('FFEEBEEFFFFF'), 210)
+        self.assertEqual(checkout_solution.checkout('HHHH'), 40)
+        self.assertEqual(checkout_solution.checkout('HHHHH'), 45)
+        self.assertEqual(checkout_solution.checkout('HHHHHH'), 55)
 
+        self.assertEqual(checkout_solution.checkout('HHHHHHHHHH'), 80)
+        self.assertEqual(checkout_solution.checkout('HHHHHHHHHHHHHHHHHHHH'), 160)
+        self.assertEqual(checkout_solution.checkout('HHHHHHHHHHHHHHHHHHHHH'), 170)
+        self.assertEqual(checkout_solution.checkout('HHHHHHHHHHHHHHHHHHHHHHHHH'), 205)
+
+        self.assertEqual(checkout_solution.checkout('K'), 80)
+        self.assertEqual(checkout_solution.checkout('KK'), 150)
+        self.assertEqual(checkout_solution.checkout('KKK'), 230)
+        self.assertEqual(checkout_solution.checkout('KKKK'), 300)
+
+        self.assertEqual(checkout_solution.checkout('NN'), 80)
+        self.assertEqual(checkout_solution.checkout('NNN'), 120)
+        self.assertEqual(checkout_solution.checkout('NNNNM'), 160)
+        self.assertEqual(checkout_solution.checkout('NNMNM'), 135)
+
+        self.assertEqual(checkout_solution.checkout('NNMNNNNN'), 280)
+        self.assertEqual(checkout_solution.checkout('MMMMNNN'), 165)
+        self.assertEqual(checkout_solution.checkout('PPPP'), 200)
+        self.assertEqual(checkout_solution.checkout('PPPPP'), 200)
+
+        self.assertEqual(checkout_solution.checkout('PPPPPPPPPPP'), 450)
+        self.assertEqual(checkout_solution.checkout('QQ'), 60)
+        self.assertEqual(checkout_solution.checkout('QQQ'), 80)
+        self.assertEqual(checkout_solution.checkout('QQQQ'), 110)
+
+        self.assertEqual(checkout_solution.checkout('QQQQQQ'), 160)
+        self.assertEqual(checkout_solution.checkout('QQQQQQQQQ'), 240)
+        self.assertEqual(checkout_solution.checkout('QQQQQQQQQQ'), 270)
+        self.assertEqual(checkout_solution.checkout('VV'), 90)
+
+        self.assertEqual(checkout_solution.checkout('VVV'), 130)
+        self.assertEqual(checkout_solution.checkout('VVVV'), 180)
+        self.assertEqual(checkout_solution.checkout('VVVVV'), 220)
+        self.assertEqual(checkout_solution.checkout('VVVVVV'), 260)
 
 if __name__ == '__main__':
     unittest.main()
